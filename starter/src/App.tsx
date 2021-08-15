2fb5bffea32cf49964d9f0a6d8ce16ae06b98c84
Krzysztof Palacz <krzysztof.a.palacz@gmail.com>
2021-08-15 09:28:55 -0700

    now with shell

--- starter/src/App.tsx
+++ starter/src/App.tsx
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  // const url = "https://www.youtube.com/embed/zFRXCwdPD-M";
   const url = 'https://competent-meninsky-9ba18d.netlify.app';
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
         <p>
-          <iframe title="embedded" src={url}></iframe>
+          <iframe title="embedded" frameBorder={0} src={url}></iframe>
         </p>
         <a
           className="App-link"
@@ -17,7 +15,6 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
     </div>
